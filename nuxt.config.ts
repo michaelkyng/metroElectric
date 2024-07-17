cbf415d16e6809693f1e961b3c157bcc67dfaabe
Michael Akpuofoba <67930856+michaelkyng@users.noreply.github.com>
2024-07-17 16:18:35 +0100

    GitHub Pages Reversed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "/nuxt-github-pages/",
-    buildAssetsDir: "assets",
+    head: {
+      charset: "UTF-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Metro Electric",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Leading the Electric Vehicle Revolution across the Continent",
+        },
+      ],
+    },
   },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/motion/nuxt"],
   devtools: { enabled: true },
