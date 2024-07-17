dfb4477363224d96040269cc2bb9016db5327005
Michael Akpuofoba <67930856+michaelkyng@users.noreply.github.com>
2024-07-17 16:07:44 +0100

    Github Pages Requirment Added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/nuxt-github-pages/",
+    buildAssetsDir: "assets",
+  },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/motion/nuxt"],
   devtools: { enabled: true },
   compatibilityDate: "2024-07-02",
